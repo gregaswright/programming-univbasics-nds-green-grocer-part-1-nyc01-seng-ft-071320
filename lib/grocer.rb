57266c2044dd5a2e8823cabaab90e7ff0193090e
Gregory A.S. Wright <geslim@gmail.com>
2020-07-09 15:28:09 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,13 +2,11 @@ require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
   matches = nil
-  collection.each do |item|
+  collection.map do |item|
     if item[:item] == name
-      matches = item
-
+      item
     end
   end
-  matches
 end
 
 def consolidate_cart(cart)
