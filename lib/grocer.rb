c9daedaeb3d01c5dfeb14f93f620ce85755483f4
Gregory A.S. Wright <geslim@gmail.com>
2020-07-09 15:27:22 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -4,7 +4,7 @@ def find_item_by_name_in_collection(name, collection)
   matches = nil
   collection.each do |item|
     if item[:item] == name
-      matches = item[:item]
+      matches = item
 
     end
   end
