0bc938840fc78ba295b11bdc1f5d3ad3e9a791bd
Gregory A.S. Wright <geslim@gmail.com>
2020-07-09 13:35:40 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,8 +1,8 @@
 require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
-  collection.each do |item|
-    if name 
+  collection[:item].each do |item|
+    if name
       binding.pry
     end
 
